dba2582d86cfb6638ea7ed8bdebb705df4e232d6
vyara <vyara_hristova@yahoo.de>
2016-02-15 19:34:05 +0200

    registered autofac in Global.asax.cs

--- Source/Web/SciHub.Web/App_Start/AutofacConfig.cs
+++ Source/Web/SciHub.Web/App_Start/AutofacConfig.cs
@@ -1,11 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace SciHub.Web.App_Start
+﻿namespace SciHub.Web
 {
-    public class AutofacConfig
+    using System.Data.Entity;
+    using System.Reflection;
+    using Autofac;
+    using Autofac.Integration.Mvc;
+    using Data;
+    using System.Web.Mvc;
+
+    public static class AutofacConfig
     {
+        public static void RegisterAutofac()
+        {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers.
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // OPTIONAL: Register model binders that require DI.
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+
+            // OPTIONAL: Register web abstractions like HttpContextBase.
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // OPTIONAL: Enable property injection in view pages.
+            builder.RegisterSource(new ViewRegistrationSource());
+
+            // OPTIONAL: Enable property injection into action filters.
+            builder.RegisterFilterProvider();
+
+            // Register services
+            RegisterServices(builder);
+
+            // Set the dependency resolver to be Autofac.
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+
+        private static void RegisterServices(ContainerBuilder builder)
+        {
+        }
     }
 }
