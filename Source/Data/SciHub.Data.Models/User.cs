94f7e2a6b29f6c33bbf0132e179f20c6685d8b30
vyara <vyara_hristova@yahoo.de>
2016-02-16 11:28:08 +0200

    added IAuditInfo and IDeletableEntity to User Model

--- Source/Data/SciHub.Data.Models/User.cs
+++ Source/Data/SciHub.Data.Models/User.cs
@@ -1,16 +1,30 @@
 ﻿namespace SciHub.Data.Models
 {
+    using System;
     using System.Security.Claims;
     using System.Threading.Tasks;
+    using Common.Models;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
 
-    public class User : IdentityUser
+    public class User : IdentityUser, IAuditInfo, IDeletableEntity 
     {
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
         {
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             return userIdentity;
         }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool PreserveCreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsHidden { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
