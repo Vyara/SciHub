9642ed64b2f86de5d49a1816e8ebeb058c513c5c
vyara <vyara_hristova@yahoo.de>
2016-02-17 23:05:17 +0200

    added automapper configuation

---
+++ Source/Web/SciHub.Web.Infrastructure/Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace SciHub.Web.Infrastructure.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IMapperConfiguration configuration);
+    }
+}
