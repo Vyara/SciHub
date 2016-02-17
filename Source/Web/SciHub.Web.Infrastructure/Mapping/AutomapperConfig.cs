f811698c432c2822332b9336b64297393d7d63b7
vyara <vyara_hristova@yahoo.de>
2016-02-17 23:14:29 +0200

    registered automapper in Global.asax.cs

--- Source/Web/SciHub.Web.Infrastructure/Mapping/AutomapperConfig.cs
+++ Source/Web/SciHub.Web.Infrastructure/Mapping/AutomapperConfig.cs
@@ -6,7 +6,7 @@
     using System.Reflection;
     using AutoMapper;
 
-    public class AutomapperConfig
+    public class AutoMapperConfig
     {
         public static MapperConfiguration Configuration { get; private set; }
 
