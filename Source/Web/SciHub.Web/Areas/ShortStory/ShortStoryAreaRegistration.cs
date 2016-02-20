ce994226f1cf1b46b0096755f1b1d0829ecfa2b6
vyara <vyara_hristova@yahoo.de>
2016-02-20 18:59:56 +0200

    scaffolding areas for admin, user, shortstory, tvshow, movie and book

---
+++ Source/Web/SciHub.Web/Areas/ShortStory/ShortStoryAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SciHub.Web.Areas.ShortStory
+{
+    public class ShortStoryAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ShortStory";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ShortStory_default",
+                "ShortStory/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Source/Web/SciHub.Web/Areas/TvShow/TvShowAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SciHub.Web.Areas.TvShow
+{
+    public class TvShowAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TvShow";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TvShow_default",
+                "TvShow/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
